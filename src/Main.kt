3d09814d32efa62d152cfb682d0c356193a262ba
Ramiz Raja <informramiz@gmail.com>
2019-04-07 05:18:27 +0500

    refactor: Rwrite improved disc intersection sol

--- src/Main.kt
+++ src/Main.kt
@@ -1,12 +1,8 @@
 import problems.Sorting
 
 fun main() {
-//    val array = arrayOf(1, 5, 2, 1, 4, 0)
-////    val array = arrayOf(-3, 1, 2, -2, 5, 6)
-//    val intersections = Sorting.calculateDiscIntersections(array)
-//    println("intersections: $intersections")
-
-    val array = arrayOf(1, 2, 3, 4, 5, 6)
-    val index = Sorting.binarySearch(array, 4)
-    println("binary search result: $index")
+    val array = arrayOf(1, 5, 2, 1, 4, 0)
+//    val array = arrayOf(-3, 1, 2, -2, 5, 6)
+    val intersections = Sorting.calculateDiscIntersections(array)
+    println("intersections: $intersections")
 }
