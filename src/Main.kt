f7bf54d9a3482573d7d7971c27e0a00fcc28db75
Ramiz Raja <informramiz@gmail.com>
2019-04-10 13:17:14 +0500

    fix: Count alive fishes prob sol

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,8 @@
 import problems.StacksAndQueues
 
 fun main() {
-    val A = arrayOf(4, 3, 2, 1, 5)
-    val B = arrayOf(0, 1, 0, 0, 0)
+    val A = arrayOf(4, 10, 2, 1, 5)
+    val B = arrayOf(0, 1, 1, 1, 0)
     val fishesAlive = StacksAndQueues.calculateAliveFishes(A, B)
     println("fishes alive at the end: $fishesAlive")
 }
--- src/problems/StacksAndQueues.kt
+++ src/problems/StacksAndQueues.kt
@@ -9,25 +9,49 @@ object StacksAndQueues {
     fun calculateAliveFishes(A: Array<Int>, B: Array<Int>): Int {
         val totalFishes = A.size
         var eatenFishes = 0
-        val stack = Stack<Int>()
+        val downstreamFishesStack = Stack<Int>()
 
         for (i in 0 until A.size) {
             if (B[i] == 1) {
                 //downstream
-                stack.push(i)
-            } else if (stack.isNotEmpty()) {
-                eatenFishes++
-                if (A[stack.peek()] < A[i]) {
-                    stack.pop()
-                }
+                downstreamFishesStack.push(i)
+                continue
+            }
 
-                while (stack.isNotEmpty() && A[stack.peek()] < A[i]) {
-                    eatenFishes++
-                    stack.pop()
+            if (downstreamFishesStack.isNotEmpty()) {
+                //encountered an upstream fish and there is a fish flowing downstream already
+                //one will eat the other
+                eatenFishes++
+
+                //the bigger fish eats the smaller fish
+                if (A[downstreamFishesStack.peek()] < A[i]) {
+                    //fish flowing upstream (0) is bigger so it will eat the fish with smaller one flowing downstream(1)
+                    downstreamFishesStack.pop()
+
+                    //fish flowing upstream will eat all smaller fishes flowing downstream
+                    eatenFishes += eatSmallerFishesMovingDownstream(A, downstreamFishesStack, i)
+
+                    //fish flowing upstream has eaten all the fishes it can
+                    // so now if there are still fishes moving downstream then the closest fish
+                    //is bigger than the fish moving upstream so the fish moving upstream will be eaten
+                    if (downstreamFishesStack.isNotEmpty()) {
+                        eatenFishes++
+                    }
                 }
             }
         }
 
         return totalFishes - eatenFishes
+    }
+
+    private fun eatSmallerFishesMovingDownstream(A: Array<Int>, stack: Stack<Int>, i: Int): Int {
+        var eatenFishesCount = 0
+        //fish flowing upstream will eat all smaller fishes flowing downstream
+        while (stack.isNotEmpty() && A[stack.peek()] < A[i]) {
+            eatenFishesCount++
+            stack.pop()
+        }
+
+        return eatenFishesCount
     }
 }
