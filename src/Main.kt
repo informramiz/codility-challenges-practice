6bfdf6f2651b66dd142d0a8152ad417083ed4e03
Ramiz Raja <informramiz@gmail.com>
2019-04-16 12:49:38 +0500

    feat: Write optimized sol for leader prob

--- src/Main.kt
+++ src/Main.kt
@@ -2,6 +2,6 @@ import problems.Leader
 
 fun main() {
     val A = arrayOf(6, 8, 4, 6, 8, 6, 6)
-    val leader = Leader.findLeaderSolution1(A)
+    val leader = Leader.findLeaderUsingStack(A)
     println("leader: $leader")
 }
--- src/problems/Leader.kt
+++ src/problems/Leader.kt
@@ -19,7 +19,7 @@ object Leader {
      * than half the total values in the sequence, there are more leader values than will fit on either
      * side of the central element in the sequence.
      */
-    fun findLeaderSolution1(A: Array<Int>): Int {
+    fun findLeaderUsingSorting(A: Array<Int>): Int {
         A.sort()
         //as array is sorted so if there is any leader it must be at the middle at has to occur more than n/2 times
         val candidate = A[A.size / 2]
@@ -32,5 +32,59 @@ object Leader {
         }
 
         return if (count > A.size/2) candidate else -1
+    }
+
+    /**
+     * Find leader using Stack: O(n)
+     * If we remove two DIFFERENT elements from the sequence then leader for remaining sequence of size n-2 is still the
+     * the same. Using this idea we can keep removing pairs of different elements and at the end if the element left
+     * is the candidate for leader.
+     *
+     * How to remove element pair?
+     * After each such operation we check whether the two elements at the top of the stack are different.
+     * If they are, we remove them from the stack. This is equivalent to removing a pair of different
+     * elements from the sequence
+     *
+     * Note: We don't need to actually use the stack data structure. All we need to remember is the last element
+     * pushed to stack (last stack top) and the current size of the stack. Then in current iteration we can
+     * compare the stack top with the current element to see if they are different and +/- the size of the stack
+     * accordingly
+     */
+    fun findLeaderUsingStack(A: Array<Int>): Int {
+        var stackSize = 0
+        var stackTop = -1
+        A.forEach { value ->
+            if (stackSize == 0) {
+                //stack is empty so push the element onto stack
+                stackTop = value
+                stackSize++
+            }  else {
+                //there is an element on stack so let's check if current element and last element are different
+                if (stackTop != value) {
+                    //decrease the size of stack by 1 (1 because we have not pushed current element to stack)
+                    //and the pair we are removing includes (current element, last element).
+                    stackSize--
+                } else {
+                    //last element and current are same so push current element to stack as well
+                    //Note: We are not updating stackTop because last and current elements are same so no need
+                    stackSize++
+                }
+            }
+        }
+
+        if (stackSize == 0) {
+            //no element left on stack so no leader candidate
+            return -1
+        }
+
+        val candidate = stackTop
+        var candidateCount = 0
+        A.forEach { value ->
+            if (value == candidate) {
+                candidateCount++
+            }
+        }
+
+        return if (candidateCount > A.size / 2) candidate else -1
     }
 }
