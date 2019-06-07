27e0b3bf01932a383f86baeaa26727474f28fa6c
Ramiz Raja <informramiz@gmail.com>
2019-06-08 02:06:29 +0500

    feat: Solve optimal board size problem

--- src/problems/BinarySearch.kt
+++ src/problems/BinarySearch.kt
@@ -44,4 +44,53 @@ object BinarySearch {
         //we did not find a match for the key
         return result
     }
+
+    /**
+     * https://codility.com/media/train/12-BinarySearch.pdf
+     *
+     * Problem:
+     * You are given n binary values x0, x1, . . . , xn−1, such that xi ∈ {0, 1}. This array represents holes in a
+     * roof (1 is a hole). You are also given k boards of the same size. The goal is to choose the optimal (minimal)
+     * size of the boards that allows all the holes to be covered by boards.
+     *
+     * Solution:
+     * We can search for a the right size of board in range [1, n] using binary search and for each size k we
+     * can check if it covers all the holes (nlgn)
+     *
+     * @param A, Ai ∈ {0, 1}, represents holes in a roof (1 is a hole)
+     * @param maxBoardsCount, boards of count
+     *
+     * @return min/optimal size of the boards (count <= maxBoardsCount) needed to cover all the holes in the roof
+     */
+    fun findBoardSize(A: Array<Int>, maxBoardsCount: Int): Int {
+        var result = -1
+        //search for right board size in range [0, A.size] using binary search
+        var start = 0
+        var end = A.lastIndex
+        while (start <= end) {
+            val mid = (start + end) / 2
+            if (countBoardsOf(mid, A) <= maxBoardsCount) {
+                end = mid - 1
+                result = end
+            } else {
+                start = mid + 1
+            }
+        }
+        return result
+    }
+
+    private fun countBoardsOf(boardSize: Int, A: Array<Int>): Int {
+        var boardsCount = 0
+        var lastCoveredIndex = -1
+        for (i in 0 until A.size) {
+            if (A[i] == 1 && i > lastCoveredIndex) {
+                //there is a hole at position i and it is not covered by previous board
+                //minus 1 because i itself is not covered and needs to be covered as well.
+                lastCoveredIndex = i + boardSize - 1
+                boardsCount++
+            }
+        }
+
+        return boardsCount
+    }
 }
