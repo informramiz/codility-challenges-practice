9842075f0367bca05837b267248d67c821950fe6
Ramiz Raja <informramiz@gmail.com>
2019-06-12 04:02:33 +0500

    feat: Solve nailPlanks problem

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,9 @@
 import problems.BinarySearch
 
 fun main() {
-    print(BinarySearch.canNailAllPlanks(arrayOf(1, 4), arrayOf(4, 6), arrayOf(4, 6), 1))
+    val A = arrayOf(1, 4, 5, 8)
+    val B = arrayOf(4, 5, 9, 10)
+    val C = arrayOf(4, 6, 7, 10, 2)
+    val nailsCount = BinarySearch.findMinNailsToPlank(A, B, C)
+    print(nailsCount)
 }
--- src/problems/BinarySearch.kt
+++ src/problems/BinarySearch.kt
@@ -207,7 +207,76 @@ object BinarySearch {
         return blockCount
     }
 
-    fun canNailAllPlanks(plankStarts: Array<Int>, plankEnds: Array<Int>,
+    /**
+     * -----------NailingPlanks--------------
+     * Count the minimum number of nails that allow a series of planks to be nailed.
+     * You are given two non-empty arrays A and B consisting of N integers. These arrays represent N planks. More precisely, A[K] is the start and B[K] the end of the K−th plank.
+     * Next, you are given a non-empty array C consisting of M integers. This array represents M nails. More precisely, C[I] is the position where you can hammer in the I−th nail.
+     * We say that a plank (A[K], B[K]) is nailed if there exists a nail C[I] such that A[K] ≤ C[I] ≤ B[K].
+     * The goal is to find the minimum number of nails that must be used until all the planks are nailed. In other words, you should find a value J such that all planks will be nailed after using only the first J nails. More precisely, for every plank (A[K], B[K]) such that 0 ≤ K < N, there should exist a nail C[I] such that I < J and A[K] ≤ C[I] ≤ B[K].
+     * For example, given arrays A, B such that:
+     * A[0] = 1    B[0] = 4
+     * A[1] = 4    B[1] = 5
+     * A[2] = 5    B[2] = 9
+     * A[3] = 8    B[3] = 10
+     * four planks are represented: [1, 4], [4, 5], [5, 9] and [8, 10].
+     * Given array C such that:
+     * C[0] = 4
+     * C[1] = 6
+     * C[2] = 7
+     * C[3] = 10
+     * C[4] = 2
+     * if we use the following nails:
+     * 0, then planks [1, 4] and [4, 5] will both be nailed.
+     * 0, 1, then planks [1, 4], [4, 5] and [5, 9] will be nailed.
+     * 0, 1, 2, then planks [1, 4], [4, 5] and [5, 9] will be nailed.
+     * 0, 1, 2, 3, then all the planks will be nailed.
+     * Thus, four is the minimum number of nails that, used sequentially, allow all the planks to be nailed.
+     * Write a function:
+     * class Solution { public int solution(int[] A, int[] B, int[] C); }
+     * that, given two non-empty arrays A and B consisting of N integers and a non-empty array C consisting of M integers, returns the minimum number of nails that, used sequentially, allow all the planks to be nailed.
+     * If it is not possible to nail all the planks, the function should return −1.
+     * For example, given arrays A, B, C such that:
+     * A[0] = 1    B[0] = 4
+     * A[1] = 4    B[1] = 5
+     * A[2] = 5    B[2] = 9
+     * A[3] = 8    B[3] = 10
+     * C[0] = 4
+     * C[1] = 6
+     * C[2] = 7
+     * C[3] = 10
+     * C[4] = 2
+     * the function should return 4, as explained above.
+     * Write an efficient algorithm for the following assumptions:
+     * N and M are integers within the range [1..30,000];
+     * each element of arrays A, B, C is an integer within the range [1..2*M];
+     * A[K] ≤ B[K].
+     *
+     */
+    fun findMinNailsToPlank(A: Array<Int>, B: Array<Int>, C: Array<Int>): Int {
+        var minNails = 1
+        var maxNails = C.size
+        //find the right number of nails using binary search
+        var minNailsCount = -1
+        while (minNails <= maxNails) {
+            val mid = (minNails + maxNails) / 2
+            if (canNailAllPlanks(A, B, C, mid)) {
+                //let's check if we can still nail if we decrease the number of nails
+                //search space: [minNails, mid-1]
+                maxNails = mid - 1
+                //keep the last nails used in case we don't find any further valid nails
+                minNailsCount = mid
+            } else {
+                //we were not able to nail all planks. We need more nails than mid
+                //search space: [mid+1, maxNails]
+                minNails = mid + 1
+            }
+        }
+
+        return minNailsCount
+    }
+
+    private fun canNailAllPlanks(plankStarts: Array<Int>, plankEnds: Array<Int>,
                          nails: Array<Int>, maximumAllowedNails: Int): Boolean {
         for (i in 0 until plankStarts.size) {
             if (!nailPlank(plankStarts[i], plankEnds[i], nails, maximumAllowedNails)) {
